da21a4ed8f910870ca4b28c6085ef9e9aabb71bf
Yohan Lee <swj03095@naver.com>
2022-08-22 02:21:26 +0900

    Lv02_07 solve

--- Algorithm/inflearn/L02/L02_07_점수계산.java
+++ Algorithm/inflearn/L02/L02_07_점수계산.java
@@ -1,5 +1,7 @@
 package inflearn.L02;
 
+import java.util.Scanner;
+
 /**
  * Created by Yohan lee Created on 2022/08/22.
  * OX 문제는 맞거나 틀린 두 경우의 답을 가지는 문제를 말한다.
@@ -8,15 +10,36 @@ package inflearn.L02;
  * 또한, 연속으로 문제의 답이 맞는 경우에서 두 번째 문제는 2점, 세 번째 문제는 3점, ..., K번째 문제는 K점으로 계산한다. 틀린 문제는 0점으로 계산한다.
  * 예를 들어, 아래와 같이 10 개의 OX 문제에서 답이 맞은 문제의 경우에는 1로 표시하고, 틀린 경우에는 0으로 표시하였을 때,
  * 점수 계산은 아래 표와 같이 계산되어, 총 점수는 1+1+2+3+1+2=10 점이다.
+ * Input
+ * 첫째 줄에 문제의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에는 N개 문제의 채점 결과를 나타내는 0 혹은 1이 빈 칸을 사이에 두고 주어진다.
+ * 0은 문제의 답이 틀린 경우이고, 1은 문제의 답이 맞는 경우이다.
+ * Output
+ * 첫째 줄에 입력에서 주어진 채점 결과에 대하여 가산점을 고려한 총 점수를 출력한다.
  **/
 public class L02_07_점수계산 {
 
     public int solution(int n, int[] arr) {
         int result = 0;
+        int accScore = 0;
+        for (int i : arr) {
+            if(i ==1) {
+                accScore++;
+                result += accScore;
+            } else {
+                accScore = 0;
+            }
+        }
         return result;
     }
 
     public static void main(String[] args) {
-
+        L02_07_점수계산 program = new L02_07_점수계산();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(program.solution(n, arr));
     }
 }
