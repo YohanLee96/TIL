886af82322c792c8c7d65d41dd3a9534fa9c1aca
Yohan Lee <swj03095@naver.com>
2022-10-11 13:34:53 +0900

    Kotiln강의 8강 스터디 완료.

--- Kotlin/inflearn/introduction/src/main/java/com/lannstark/lec08/Lec08Main.java
+++ Kotlin/inflearn/introduction/src/main/java/com/lannstark/lec08/Lec08Main.java
@@ -2,14 +2,38 @@ package com.lannstark.lec08;
 
 public class Lec08Main {
 
-  public static void main(String[] args) {
-
-  }
-
-  public static void printAll(String... strings) {
-    for (String str : strings) {
-      System.out.println(str);
+    public static void main(String[] args) {
     }
-  }
+
+    int max(int a, int b) {
+        if (a > b) {
+            return a;
+        }
+
+        return b;
+    }
+
+    void repeat(String str) {
+        repeat(str, 3, true);
+    }
+
+    void repeat(String str, int num) {
+        repeat(str, num, true);
+    }
+    void repeat(String str, int num, boolean useNewLine) {
+        for (int i = 1; i <= num; i++) {
+            if (useNewLine) {
+                System.out.println(str);
+            } else {
+                System.out.print(str);
+            }
+        }
+    }
+
+    public static void printAll(String... strings) {
+        for (String str : strings) {
+            System.out.println(str);
+        }
+    }
 
 }
--- Kotlin/inflearn/introduction/src/main/java/com/lannstark/lec08/Lec08MainKt.kt
+++ Kotlin/inflearn/introduction/src/main/java/com/lannstark/lec08/Lec08MainKt.kt
@@ -6,5 +6,50 @@ package com.lannstark.lec08
  **/
 
 fun main() {
+//    repeat("abc", useNewLine = false);
+//    printNameAndGender(name = "FEMALE", gender = "최태현")
 
+    printAll("A", "B", "C")
+    val array = arrayOf("A", "B", "C")
+    printAll(*array)
+}
+
+fun max(a: Int, b: Int): Int {
+    return if (a > b) {
+        a
+    } else {
+        b
+    }
+}
+
+fun max2(a: Int, b: Int): Int =
+    if (a > b) {
+        a
+    } else { b }
+
+fun max3(a: Int, b: Int) = if (a > b) a else b
+
+fun repeat(
+    str: String,
+    num: Int = 3,
+    useNewLine: Boolean = true
+) {
+    for(i in 1..num) {
+        if(useNewLine) {
+            println(str)
+        } else {
+            print(str)
+        }
+    }
+}
+
+fun printNameAndGender(name: String, gender: String) {
+    println(name)
+    println(gender)
+}
+
+fun printAll(vararg strings: String) {
+    for (str in strings) {
+        println(str)
+    }
 }
