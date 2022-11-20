4e332c946be7b000490d40096e32a5c570f7053a
Yohan Lee <swj03095@naver.com>
2022-11-20 21:02:32 +0900

    L06_02 solve.

--- Algorithm/inflearn/L06/L06_02_버블정렬.java
+++ Algorithm/inflearn/L06/L06_02_버블정렬.java
@@ -3,13 +3,28 @@ package inflearn.L06;
 import java.util.Scanner;
 
 /**
- * Created by Yohan lee Created on 2022/11/20. N개의 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요. 정렬하는 방법은 버블정렬입니다.
- * <p>
- * Input 첫 번째 줄에 자연수 N(1<=N<=100)이 주어집니다. 두 번째 줄에 N개의 자연수가 공백을 사이에 두고 입력됩니다. 각 자연수는 정수형 범위 안에 있습니다. Output 오름차순으로 정렬된 수열을 출력합니다.
+ * Created by Yohan lee Created on 2022/11/20.
+ * N개의 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요. 정렬하는 방법은 버블정렬입니다.
+ *
+ * Input
+ * 첫 번째 줄에 자연수 N(1<=N<=100)이 주어집니다.
+ * 두 번째 줄에 N개의 자연수가 공백을 사이에 두고 입력됩니다.
+ * 각 자연수는 정수형 범위 안에 있습니다.
+ * Output
+ * 오름차순으로 정렬된 수열을 출력합니다.
  **/
 public class L06_02_버블정렬 {
 
     public int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n-1; i++) { //마지막 인덱스는 최초 탐색 시, 제일 높은값으로 정해지므로 탐색할 필요 없음.
+            for (int j = 0; j < n -i -1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
         return arr;
     }
 
