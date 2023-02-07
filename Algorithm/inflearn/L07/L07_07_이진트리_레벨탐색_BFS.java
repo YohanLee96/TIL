52f870227d05f2f55ef027bbff1b0ad13cb02851
Yohan Lee <swj03095@naver.com>
2023-02-08 00:41:28 +0900

    L07_07 solve.

--- Algorithm/inflearn/L07/L07_07_이진트리_레벨탐색_BFS.java
+++ Algorithm/inflearn/L07/L07_07_이진트리_레벨탐색_BFS.java
@@ -1,5 +1,8 @@
 package inflearn.L07;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by Yohan lee
  * Created on 2023/02/06.
@@ -7,7 +10,59 @@ package inflearn.L07;
  **/
 public class L07_07_이진트리_레벨탐색_BFS {
 
-    public static void main(String[] args) {
+    static Node root;
 
+    public static void BFS(Node root) {
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(root);
+        int L = 0; // root Node는 1
+        while (!Q.isEmpty()) { //비어있을때까지 루프
+            int len = Q.size();
+            System.out.print(L + " -> ");
+            for(int i=0; i<len; i++) {
+                Node current = Q.poll();
+                System.out.print(current.data + " ");
+                if(current.lt != null) {
+                    Q.offer(current.lt);
+                }
+                if(current.rt != null) {
+                    Q.offer(current.rt);
+                }
+            }
+            L++;
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+        root.rt.lt = new Node(6);
+        root.rt.rt = new Node(7);
+        root.lt.lt.lt = new Node(8);
+        root.lt.lt.rt = new Node(9);
+        root.lt.rt.lt = new Node(10);
+        root.lt.rt.rt = new Node(11);
+        root.rt.lt.lt = new Node(12);
+        root.rt.lt.rt = new Node(13);
+        root.rt.rt.lt = new Node(14);
+        root.rt.rt.rt = new Node(15);
+        BFS(root);
+    }
+
+
+    static class Node {
+         int data;
+         Node lt;
+         Node rt;
+
+        public Node(int data) {
+            this.data = data;
+            this.lt = null;
+            this.rt = null;
+        }
     }
 }
