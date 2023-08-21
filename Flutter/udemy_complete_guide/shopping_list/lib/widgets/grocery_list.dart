536612b8c4c7804516c29b223f89f7c35c97da46
Yohan Lee <swj03095@naver.com>
2023-08-22 02:11:56 +0900

    Flutter : 207강

--- Flutter/udemy_complete_guide/shopping_list/lib/widgets/grocery_list.dart
+++ Flutter/udemy_complete_guide/shopping_list/lib/widgets/grocery_list.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _addItem(context);
+            },
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
@@ -19,7 +41,7 @@ class GroceryList extends StatelessWidget {
             height: 24,
             color: groceryItems[index].category.color,
           ),
-          trailing: Text(groceryItems[index].quantity.toString()                                           ),
+          trailing: Text(groceryItems[index].quantity.toString()),
         ),
       ),
     );
