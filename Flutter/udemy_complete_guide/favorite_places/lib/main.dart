d567007cef87048153159cb7a470a014c73a9464
Yohan Lee <swj03095@naver.com>
2023-09-19 02:27:46 +0900

    Flutter : ~238강

--- Flutter/udemy_complete_guide/favorite_places/lib/main.dart
+++ Flutter/udemy_complete_guide/favorite_places/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:favorite_places/screens/places.dart';
-import 'package:favorite_places/widgets/places_list.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
 
 final colorScheme = ColorScheme.fromSeed(
@@ -28,9 +27,9 @@ final theme = ThemeData().copyWith(
 );
 
 void main() {
-  runApp(
-    const MyApp(),
-  );
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
---
+++ Flutter/udemy_complete_guide/favorite_places/lib/screens/add_place.dart
@@ -0,0 +1,62 @@
+import 'package:favorite_places/providers/user_places.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AddPlaceScreen extends ConsumerStatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  ConsumerState<AddPlaceScreen> createState() {
+    return _AddPlaceScreenState();
+  }
+}
+
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
+  void _savePlace() {
+    final enteredText = _titleController.text;
+    if (enteredText.isEmpty) {
+      return;
+    }
+
+    //상태관리에 추가.
+    ref.read(userPlacesProvider.notifier).addPlace(enteredText);
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _titleController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add new Place'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton.icon(
+              onPressed: _savePlace,
+              icon: const Icon(Icons.add),
+              label: const Text('Add Place'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
