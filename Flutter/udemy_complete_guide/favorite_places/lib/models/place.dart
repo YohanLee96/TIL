7652d50a1c8bc0d422e3af9f69a452885d01c579
Yohan Lee <swj03095@naver.com>
2023-09-26 03:10:06 +0900

    Flutter : ~250강

--- Flutter/udemy_complete_guide/favorite_places/lib/models/place.dart
+++ Flutter/udemy_complete_guide/favorite_places/lib/models/place.dart
@@ -4,10 +4,27 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+class PlaceLocation {
+  const PlaceLocation({
+    required this.latitude,
+    required this.longitube,
+    required this.address,
+  });
+
+  final double latitude;
+  final double longitube;
+  final String address;
+}
+
 class Place {
-  Place({required this.title, required this.image}) : id = uuid.v4();
+  Place({
+    required this.title,
+    required this.image,
+    // required this.placeLocation,
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
   final File image;
+// final PlaceLocation placeLocation;
 }
--- Flutter/udemy_complete_guide/favorite_places/lib/widgets/location_input.dart
+++ Flutter/udemy_complete_guide/favorite_places/lib/widgets/location_input.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
+import 'package:favorite_places/models/place.dart';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -11,8 +15,17 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
-  Location? _pickedLocation;
+  PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
+
+  String get locationImage {
+    if (_pickedLocation == null) {
+      return '';
+    }
+    final lat = _pickedLocation!.latitude;
+    final lng = _pickedLocation!.longitube;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:A%7C$lat,$lng&key=AIzaSyARQ1as8mzWe0_En4ObKYPxQrvWb20mUF8';
+  }
 
   void _getCurrentLocation() async {
     Location location = Location();
@@ -42,11 +55,24 @@ class _LocationInputState extends State<LocationInput> {
     });
 
     locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
+    if (lat == null || lng == null) {
+      return;
+    }
+    final url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=AIzaSyARQ1as8mzWe0_En4ObKYPxQrvWb20mUF8');
+    final response = await http.get(url);
+    final resData = json.decode(response.body);
+    final address = resData['results'][0]['formatted_address'];
     setState(() {
+      _pickedLocation = PlaceLocation(
+        latitude: lat,
+        longitube: lng,
+        address: address,
+      );
       _isGettingLocation = false;
     });
-    print(locationData.latitude);
-    print(locationData.longitude);
   }
 
   @override
@@ -55,11 +81,20 @@ class _LocationInputState extends State<LocationInput> {
       '장소가 선택되지 않았습니다.',
       textAlign: TextAlign.center,
       style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-        color: Theme.of(context).colorScheme.onBackground,
-      ),
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
     );
 
-    if(_isGettingLocation) {
+    if (_pickedLocation != null) {
+      previewContent = Image.network(
+        locationImage,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      );
+    }
+
+    if (_isGettingLocation) {
       previewContent = const CircularProgressIndicator();
     }
 
