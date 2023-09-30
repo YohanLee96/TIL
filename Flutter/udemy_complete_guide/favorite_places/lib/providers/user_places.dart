f463e1d065cef30a1392b76f661558172cd4153c
Yohan Lee <swj03095@naver.com>
2023-09-30 23:29:23 +0900

    Flutter : 13강 완료. ~264강

--- Flutter/udemy_complete_guide/favorite_places/lib/providers/user_places.dart
+++ Flutter/udemy_complete_guide/favorite_places/lib/providers/user_places.dart
@@ -3,16 +3,61 @@ import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 import 'package:favorite_places/models/place.dart';
+
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+    return db.execute(
+        'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+  }, version: 1);
+
+  return db;
+}
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
+
+  Future<void> loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+    final places = data
+        .map(
+          (row) => Place(
+            id: row['id'] as String,
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            placeLocation: PlaceLocation(
+              latitude: row['lat'] as double,
+              longitube: row['lng'] as double,
+              address: row['address'] as String,
+            ),
+          ),
+        )
+        .toList();
+
+    state = places;
+  }
 
   void addPlace(String title, File image, PlaceLocation location) async {
     final appDir = await syspaths.getApplicationDocumentsDirectory();
     final fileName = path.basename(image.path);
     final copiedImage = await image.copy('${appDir.path}/$fileName');
-    final newPlace = Place(title: title, image: copiedImage, placeLocation: location);
+    final newPlace =
+        Place(title: title, image: copiedImage, placeLocation: location);
+
+    final db = await _getDatabase();
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.placeLocation.latitude,
+      'lng': newPlace.placeLocation.longitube,
+      'address': newPlace.placeLocation.address
+    });
     state = [newPlace, ...state];
   }
 }
--- Flutter/udemy_complete_guide/favorite_places/lib/screens/places.dart
+++ Flutter/udemy_complete_guide/favorite_places/lib/screens/places.dart
@@ -5,13 +5,27 @@ import 'package:favorite_places/screens/add_place.dart';
 import 'package:favorite_places/widgets/places_list.dart';
 import 'package:favorite_places/providers/user_places.dart';
 
-class PlacesScreen extends ConsumerWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final userPlaces = ref.watch(userPlacesProvider);
+  ConsumerState<PlacesScreen> createState() {
+    return _PlacesScreenState();
+  }
+}
 
+class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final userPlaces = ref.watch(userPlacesProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Places'),
@@ -30,9 +44,12 @@ class PlacesScreen extends ConsumerWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8),
-        child: PlacesList(
-          places: userPlaces,
-        ),
+        child: FutureBuilder(
+            future: _placesFuture,
+            builder: (context, snapshot) =>
+                snapshot.connectionState == ConnectionState.waiting
+                    ? const Center(child: CircularProgressIndicator())
+                    : PlacesList(places: userPlaces)),
       ),
     );
   }
