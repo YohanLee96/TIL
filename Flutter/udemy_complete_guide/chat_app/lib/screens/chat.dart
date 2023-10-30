e3d341fb765845fd236e32d32a37484ac7390d50
Yohan Lee <swj03095@naver.com>
2023-10-31 02:37:45 +0900

    Flutter : 완강

--- Flutter/udemy_complete_guide/chat_app/lib/screens/chat.dart
+++ Flutter/udemy_complete_guide/chat_app/lib/screens/chat.dart
@@ -1,11 +1,33 @@
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
 
+  @override
+  State<StatefulWidget> createState()  => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  void setupPushNoti() async {
+    final fcm = FirebaseMessaging.instance;
+    //권한 허가 요청.
+    await fcm.requestPermission();
+    final fcmToken = await fcm.getToken();
+    print(fcmToken);
+    fcm.subscribeToTopic('chat');
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNoti();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,8 +44,8 @@ class ChatScreen extends StatelessWidget {
               ))
         ],
       ),
-      body: Column(
-        children: const [
+      body: const Column(
+        children: [
           Expanded(
             child: ChatMessages(),
           ),
--- Flutter/udemy_complete_guide/chat_app/lib/widgets/chat_messages.dart
+++ Flutter/udemy_complete_guide/chat_app/lib/widgets/chat_messages.dart
@@ -1,15 +1,71 @@
-
+import 'package:chat_app/widgets/message_bubble.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
-
   const ChatMessages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('메시지가 없습니다.'),
+    final authenticatedUser = FirebaseAuth.instance.currentUser!;
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: true)
+          .snapshots(),
+      builder: (ctx, chatSnapshots) {
+        if (chatSnapshots.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: Text('메시지가 없습니다.'),
+          );
+        }
+        if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('메시지가 없습니다.'),
+          );
+        }
+
+        if (chatSnapshots.hasError) {
+          return const Center(
+            child: Text('에러가 발생하였습니다.'),
+          );
+        }
+
+        final loadedMessages = chatSnapshots.data!.docs;
+        return ListView.builder(
+          padding: const EdgeInsets.only(
+            bottom: 40,
+            left: 13,
+            right: 13,
+          ),
+          reverse: true, //리스트가 역순으로 UI상에 쌓임.
+          itemCount: loadedMessages.length,
+          itemBuilder: (ctx, index) {
+            final chatMessage = loadedMessages[index].data();
+            final nextChatMessage = index + 1 < loadedMessages.length
+                ? loadedMessages[index + 1].data()
+                : null;
+
+            final currentMessageUserId = chatMessage['userId'];
+            final nextMessageUserId =
+                nextChatMessage != null ? nextChatMessage['userId'] : null;
+            final nextUserIsSame = nextMessageUserId == currentMessageUserId;
+            if (nextUserIsSame) {
+              return MessageBubble.next(
+                message: chatMessage['text'],
+                isMe: authenticatedUser.uid == currentMessageUserId,
+              );
+            }
+            return MessageBubble.first(
+              userImage: chatMessage['userImage'],
+              username: chatMessage['username'],
+              message: chatMessage['text'],
+              isMe: authenticatedUser.uid == currentMessageUserId,
+            );
+          },
+        );
+      },
     );
   }
-
 }
